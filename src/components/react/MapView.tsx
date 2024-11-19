b6fa2fbcc2a3f2ba4e94d745ff98fe391d9e8939
adrian daniel gutierrez martinez <adriand@estudiantes.uci.cu>
2024-11-19 05:00:04 +0100

    Creacion del componente MapView usando react-leaflet.

---
+++ src/components/react/MapView.tsx
@@ -0,0 +1,22 @@
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import type { LatLngExpression } from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+const MAP_POSITIONS: LatLngExpression = [23.128700132889083, -82.27046820851496]
+
+export default function MapView() {
+    return (
+        <MapContainer
+            center={MAP_POSITIONS}
+            zoom={17}
+            attributionControl={false}
+            zoomControl={false}
+            className="min-w-[272px] min-h-[272px] rounded-xl z-0"
+            style={{height: '100%', width: '100%'}}
+        >
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+            <Marker position={MAP_POSITIONS} title={"Pizzeton D'Guti"} alt={'Pizzeton Ubication'} />
+        </MapContainer>
+    )
+};
+
