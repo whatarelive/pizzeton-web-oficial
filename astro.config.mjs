5d1179aa93680adc6999ad8d2ea5f3652893c46f
adrian daniel gutierrez martinez <adriand@estudiantes.uci.cu>
2024-12-26 05:35:38 +0100

    Se agrego la integración @astro/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,12 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel/serverless';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind(), react()],
+  site: "https://pizzetondguti.vercel.app",
+  integrations: [tailwind(), react(), sitemap()],
   adapter: vercel(),
 });
