9ebe29db29c22d6ec28814a4c374db43db2c4adf
adrian daniel gutierrez martinez <adriand@estudiantes.uci.cu>
2024-12-26 04:58:45 +0100

    Se actualizo el sitemap.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel/serverless';
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  site: "https://pizzeton-web-oficial.vercel.app/",
-  integrations: [tailwind(), react(), sitemap()],
+  integrations: [tailwind(), react()],
   adapter: vercel(),
 });
