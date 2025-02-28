ca33ac2b350599f83db21fd7861165eb7da7c31e
adrian daniel gutierrez martinez <adriand@estudiantes.uci.cu>
2025-02-28 07:33:48 +0100

    Corrección de los estilos del componente MenuTabs.

--- src/components/react/MenuTabs.tsx
+++ src/components/react/MenuTabs.tsx
@@ -7,7 +7,7 @@ const buttons = ['Pizzas', 'Pastas', 'Bebidas', 'Postres', 'Otros'];
 
 export default function MenuTabs({ selectTab, handleClick }: Props) {
   return (
-    <div className="container max-w-[320px] overflow-scroll flex w-full sm:max-w-screen-2xl justify-start gap-2">
+    <div className="container max-w-[320px] overflow-y-scroll flex w-full sm:overflow-y-hidden sm:max-w-screen-2xl justify-start gap-2">
         { 
             buttons.map(label => (
                 <button 
